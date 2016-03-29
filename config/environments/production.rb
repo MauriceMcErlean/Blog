e631b22596847785f016bab6921829b6859752ec
MauriceMcErlean <mauricemcerlean@hotmail.co.uk>
2016-03-29 22:18:03 +0100

    production precompile added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.assets.precompile = ['*.js', '*.css', '*.css.erb']
+  RAILS_ENV=production bundle exec rake assets:precompile
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
